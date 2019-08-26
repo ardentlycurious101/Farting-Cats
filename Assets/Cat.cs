ba5f30c1d9f80693c6ef16ffceec92497d126657
Elina Lua Ming <ardentlycurious101@gmail.com>
2019-08-26 00:01:10 -0700

    Added Cat prefab, and AudioSource.

---
+++ Assets/Cat.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        Thrust();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
+        }
+        //else // commented out because mp3 file is very short, might sound unnatural.
+        //{
+        //    audioSource.Stop();
+        //}
+    }
+}
